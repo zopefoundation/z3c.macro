5b463297b85564e8a9ed9cb734c20baaec3c6168
Stephan Richter <stephan.richter@gmail.com>
2013-02-25 02:16:44 -0500

    Back to development: 2.0.0a2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*rnames):
 
 setup(
     name='z3c.macro',
-    version='2.0.0a1',
+    version='2.0.0a2.dev0',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "Simpler definition of ZPT macros.",
