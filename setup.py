0d2208e491a90e4339619ec9ab70a36a125c083c
Michael Howitz <icemac@gmx.net>
2024-01-11 08:45:26 +0100

    Back to development: 3.1

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ TESTS_REQUIRE = CHAMELEON_REQUIRES + [
 
 setup(
     name='z3c.macro',
-    version='3.0',
+    version='3.1.dev0',
     author="Roger Ineichen and the Zope Community",
     author_email="zope-dev@zope.org",
     description="Simpler definition of ZPT macros.",
